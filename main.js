6cb07249d0fb029c6b79a6d2aa3038495d840440
stojan.milosev <stojan.milosev@yahoo.com>
2017-08-25 12:18:30 +0200

    manje CSS izmene, dodati koemntari u JS fajl

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-
-
+//definisanje promenljivih html elemenata
     var input,filter;
     var list = document.getElementById("filter-list");
     var li = list.getElementsByTagName("li");
     
   function filterList(){
+    //definisanje html elemnta za unos
     input = document.getElementById("filter-input");
+    //kovertovanje svih karaktera unosa u velika slova
     filter = input.value.toUpperCase();
-    console.log(filter);
+    //za svaki element filtrirane liste
     for(var i = 0;i<li.length;i++){
+        //vrednost sadržaja elementa liste
         name  = li[i].innerHTML;
+        //ako se vrednost promenljive filter nalazi u promenljivoj name
+        //dodaj CSS pravilo "display:block;" koja utiče na vidljivost elemnta liste
+        //promenljive "name" i "filter" su tipa string 
         if(name.toUpperCase().indexOf(filter)>-1){
             li[i].style.display = "block";
         }else{
